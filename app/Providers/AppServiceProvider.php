36f1ebbafc1c8525f057af621eee20a894a7f881
Ismoil Nosr <ismoil.nosr@gmail.com>
2020-09-21 21:48:42 +0500

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (app()->environment('production')) {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
 
         view()->composer('layout.sidebar', function ($view) {
